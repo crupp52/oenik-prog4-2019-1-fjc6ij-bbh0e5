ccf99912a5cb2e2b91e810d37758c83226c1b37a
zucklevente <zuck.levente@protonmail.com>
2019-04-13 22:47:54 +0200

    Add: Some line of code to test SaveGameState and LoadGameState method

--- OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Display/MainWindow.xaml.cs
+++ OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Display/MainWindow.xaml.cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace TronGame.Display
+﻿namespace TronGame.Display
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+    using TronGame.BusinessLogic;
+    using TronGame.Repository;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -22,7 +24,14 @@ namespace TronGame.Display
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            IRepository repository = new GameRepository();
+            repository.Player1.Name = "Teszt Béla";
+            repository.Player2.Name = "Teszt Elek";
+
+            IBusinessLogic logic = new GameLogic();
+            logic.SaveGamestate();
+            logic.LoadGamestate("save20190413222910.xml");
         }
     }
 }
