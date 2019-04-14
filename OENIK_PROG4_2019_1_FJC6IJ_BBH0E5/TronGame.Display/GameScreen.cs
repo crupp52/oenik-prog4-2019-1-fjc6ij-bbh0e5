7dc7b52bf45cd27208ad0506185f623579cd53cf
zucklevente <zuck.levente@protonmail.com>
2019-04-14 22:29:45 +0200

    Add: Implement GameScreen class

--- OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Display/GameScreen.cs
+++ OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Display/GameScreen.cs
@@ -6,11 +6,14 @@
     using System.Text;
     using System.Threading.Tasks;
     using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Threading;
     using TronGame.BusinessLogic;
+    using TronGame.Model;
 
     public class GameScreen : FrameworkElement
     {
-        private GameModel model;
+        private IGameModel model;
         private IBusinessLogic logic;
 
         public GameScreen()
@@ -19,9 +22,37 @@
             this.Loaded += this.GameScreen_Loaded;
         }
 
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+        }
+
         private void GameScreen_Loaded(object sender, RoutedEventArgs e)
         {
-            this.logic = new GameLogic();
+            this.logic = new GameLogic(this.model);
+            this.logic.ScreenRefresh += this.Logic_ScreenRefresh;
+            Window window = Window.GetWindow(this);
+            window.KeyDown += this.Window_KeyDown;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(33.3333333);
+            timer.Tick += this.Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Logic_ScreenRefresh(object sender, EventArgs e)
+        {
+            this.InvalidateVisual();
+        }
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
