8841887f71b9cbb2c34b9067296ae9a1a89cca15
zucklevente <zuck.levente@protonmail.com>
2019-04-14 22:36:20 +0200

    Modify: Cleaned the Repository

--- OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Repository/GameRepository.cs
+++ OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Repository/GameRepository.cs
@@ -21,31 +21,5 @@
 
     public class GameRepository : IRepository
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="GameRepository"/> class.
-        /// </summary>
-        public GameRepository()
-        {
-            this.Obstacles = new List<ObstacleObject>();
-            this.Turbos = new List<TurboObject>();
-            this.Player1 = new Player();
-            this.Player2 = new Player();
-            this.Difficulty = Difficulty.Medium;
-            this.HighScore = new HighScore();
-        }
-
-        public Difficulty Difficulty { get; set; }
-
-        public HighScore HighScore { get; set; }
-
-        public Player Player1 { get; set; }
-
-        public Player Player2 { get; set; }
-
-        public List<ObstacleObject> Obstacles { get; set; }
-
-        public List<TurboObject> Turbos { get; set; }
-
-        public GameObject[,] GameField { get; set; }
     }
 }
--- OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Repository/Interfaces/IRepository.cs
+++ OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Repository/Interfaces/IRepository.cs
@@ -8,18 +8,5 @@
 
     public interface IRepository
     {
-        Player Player1 { get; set; }
-
-        Player Player2 { get; set; }
-
-        List<ObstacleObject> Obstacles { get; set; }
-
-        List<TurboObject> Turbos { get; set; }
-
-        Difficulty Difficulty { get; set; }
-
-        GameObject[,] GameField { get; set; }
-
-        HighScore HighScore { get; set; }
     }
 }
