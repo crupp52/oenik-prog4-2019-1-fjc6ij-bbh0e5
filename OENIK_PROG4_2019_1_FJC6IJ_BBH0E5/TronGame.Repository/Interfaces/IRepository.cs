73d1bf89803841e0ab78e52afd1f1d5f07011110
zucklevente <zuck.levente@protonmail.com>
2019-04-24 23:21:11 +0200

    Add: documentation of IRepository.cs

--- OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Repository/Interfaces/IRepository.cs
+++ OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Repository/Interfaces/IRepository.cs
@@ -6,6 +6,9 @@
     using System.Text;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Interface for GameRepository class.
+    /// </summary>
     public interface IRepository
     {
     }
