6f81bbd5b616e1bf6bfaaba49063e03f6277c29a
fjc6ij <kinorbi95@gmail.com>
2019-05-01 21:56:25 +0200

    Add: player name changing form CS

---
+++ OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Display/ChangePlayersNameWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TronGame.Display
+{
+    /// <summary>
+    /// Interaction logic for ChangePlayersNameWindow.xaml
+    /// </summary>
+    public partial class ChangePlayersNameWindow : Window
+    {
+        private GameControl control;
+
+        public ChangePlayersNameWindow(GameControl gameControl)
+        {
+            InitializeComponent();
+
+            control = gameControl;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name1 = this.player1name.Text;
+            string name2 = this.player2name.Text;
+
+            if (name1 != string.Empty && name2 != string.Empty)
+            {
+                this.control.ChangePlayersName(name1, name2);
+                this.Close();
+            }
+        }
+    }
+}
