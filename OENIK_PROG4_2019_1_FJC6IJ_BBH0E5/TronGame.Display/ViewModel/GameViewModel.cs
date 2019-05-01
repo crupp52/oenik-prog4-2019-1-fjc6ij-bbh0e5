c5a389c74063470a88e141e66f6578793a25cf29
fjc6ij <kinorbi95@gmail.com>
2019-05-01 18:15:26 +0200

    Add: Difficulty commands in GameViewModel + set difficulty function now updates the settings.xml file

--- OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Display/ViewModel/GameViewModel.cs
+++ OENIK_PROG4_2019_1_FJC6IJ_BBH0E5/TronGame.Display/ViewModel/GameViewModel.cs
@@ -7,6 +7,7 @@
     using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Input;
+    using System.Xml.Linq;
     using GalaSoft.MvvmLight;
     using GalaSoft.MvvmLight.CommandWpf;
     using TronGame.Model;
@@ -27,6 +28,9 @@
             this.ExitGameCommand = new RelayCommand(() => { Application.Current.Shutdown(); });
             this.ShowHighScoreCommand = new RelayCommand(() => { MessageBox.Show(this.GameControl.GameModel.HighScore.GetFullDescription()); });
             this.ShowHelpWindowCommand = new RelayCommand(() => { new HelpWindow().Show(); });
+            this.SetDifficultyToEasyCommand = new RelayCommand(() => { this.SetDifficulty(1); });
+            this.SetDifficultyToMediumCommand = new RelayCommand(() => { this.SetDifficulty(2); });
+            this.SetDifficultyToHardCommand = new RelayCommand(() => { this.SetDifficulty(3); });
         }
 
         public GameControl GameControl { get; set; }
@@ -36,5 +40,19 @@
         public ICommand ShowHighScoreCommand { get; private set; }
 
         public ICommand ShowHelpWindowCommand { get; private set; }
+
+        public ICommand SetDifficultyToEasyCommand { get; private set; }
+
+        public ICommand SetDifficultyToMediumCommand { get; private set; }
+
+        public ICommand SetDifficultyToHardCommand { get; private set; }
+
+        private void SetDifficulty(int diff)
+        {
+            var xml = XDocument.Load(@"../../../TronGame.Repository/XMLs/settings.xml");
+            xml.Root.SetElementValue("difficulty", diff);
+            xml.Save(@"../../../TronGame.Repository/XMLs/settings.xml");
+            MessageBox.Show("Difficulty was modified!");
+        }
     }
 }
